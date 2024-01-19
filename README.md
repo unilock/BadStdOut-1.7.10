881069ee5c94372e7ac3402c64a6914b170f12e7
unilock <unilock@fennet.rentals>
2024-01-19 17:30:56 -0500

    init
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ README.md
@@ -0,0 +1,3 @@
+# BadStdOut-1.7.10
+
+[BadStdOut](https://github.com/Szum123321/badstdout) for Forge 1.7.10
---
+++ src/main/java/net/szum123321/badstdout/mixin/FMLRelaunchLogMixin.java
@@ -0,0 +1,17 @@
+package net.szum123321.badstdout.mixin;
+
+import cpw.mods.fml.relauncher.FMLRelaunchLog;
+import net.szum123321.badstdout.SourceAwarePrintStream;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FMLRelaunchLog.class)
+public class FMLRelaunchLogMixin {
+    @Inject(method = "Lcpw/mods/fml/relauncher/FMLRelaunchLog;configureLogging()V", at = @At("RETURN"))
+    private void configureLogging(CallbackInfo ci) {
+        System.setOut(new SourceAwarePrintStream("STDOUT", System.out));
+        System.setOut(new SourceAwarePrintStream("STDERR", System.err));
+    }
+}
